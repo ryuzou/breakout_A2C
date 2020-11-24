fix padding and softmax's dim = 1

env.py
@@ -3,23 +3,30 @@ from skimage.color import rgb2gray
 from skimage.transform import resize
 import gym
 from gym import wrappers
+from PIL import Image
 
 pic_width = 84
 pic_height = 84
 
 
 def change_pict(pict):
-    pict = pict[49:193, 8:152]  # new
-    ans = resize(rgb2gray(pict), (pic_width, pic_height))
-    return np.array(ans)
+    # pict = pict[49:193, 8:152]  # new
+    # ans = resize(rgb2gray(pict), (pic_width, pic_height))
+    # return np.array(ans)
+    frame = Image.fromarray(pict)
+    frame = frame.convert("L")
+    frame = frame.crop((0, 20, 160, 210))
+    frame = frame.resize((pic_width, pic_height))
+    frame = np.array(frame, dtype=np.float32)
+    return frame
 
 
 class gym_env:
     def __init__(self, worker_id):
         self.env = gym.make("BreakoutDeterministic-v4")
         self.env.reset()
-        if worker_id == 0:
-            self.env = wrappers.Monitor(self.env, "/home/emile/Videos/", video_callable=(lambda ep: ep % 15 == 0))
+        # if worker_id == 0:
+        #     self.env = wrappers.Monitor(self.env, "/home/emile/Videos/", video_callable=(lambda ep: ep % 15 == 0))
         self.life = -1
         self.fire_action = 1
 

main2.py
@@ -23,6 +23,7 @@ frame_siz = 4
 Worker_num = 16
 alpha = 0.5
 beta = 0.005 # 0.01
+# beta = 0.01
 gamma = 0.99
 pic_width = 84
 pic_height = 84
@@ -30,6 +31,7 @@ convert_shape = (-1, frame_siz, pic_width, pic_height)
 import_flag = False
 flag_google = False
 export_flag = False
+
 
 @dataclass
 class step_info:
@@ -117,6 +119,13 @@ class Workers:
         step = self.self_pips[worker_id].recv()
         return step
 
+    def step2(self, worker_id, action):
+        self.self_pips[worker_id].send(("step", action))
+        step = self.self_pips[worker_id].recv()
+        if (step.done or step.info):
+            step.reward = -1.0
+        return step
+
     def reset(self, worker_id):
         self.self_pips[worker_id].send(("reset", None))
         return self.self_pips[worker_id].recv()
@@ -182,7 +191,7 @@ class Agent:
     def __init__(self):
         self.network = Network(action_space=4, step_repeat_times=frame_siz, alpha=alpha, beta=beta)
         self.workers = Workers(num_workers=Worker_num)
-        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=0.00005)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=1e-4)
         self.global_score = collections.deque(maxlen=50)
         self.w_states = [self.workers.get_state(i) for i in range(self.workers.num_workers)]  # states of each workers.
         self.w_stop = []  # if true then do not worker.step()
@@ -200,7 +209,6 @@ class Agent:
                 self.network.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
             with open(data_path, 'rb') as wb:
                 self.train_time = pickle.load(wb)
-
 
     def calc_drew(self, rews, last_state):  # last_state は時系列的に最後のstate (n_states[-1]でok)
         d_rews = []
@@ -250,7 +258,7 @@ class Agent:
 
         for i, state in enumerate(now_states):  # 行動選択
             act = self.network.select_action2(state)
-            step = self.workers.step(i, act[0])
+            step = self.workers.step2(i, act[0])
             states.append(step.state)
             acts.append([step.action])
             rews.append([step.reward])
@@ -286,8 +294,9 @@ class Agent:
             for d_rew in d_rews_tmp:
                 d_rews_ans.append(d_rew)
 
-        states = self.reshaper_v(states).reshape((self.workers.num_workers * trajectory_size, 1, frame_siz, pic_width, pic_height))
-        return states, d_rews_ans, self.reshaper_h(acts).tolist()#, self.reshaper_h(probs).tolist()
+        states = self.reshaper_v(states).reshape(
+            (self.workers.num_workers * trajectory_size, 1, frame_siz, pic_width, pic_height))
+        return states, d_rews_ans, self.reshaper_h(acts).tolist()  # , self.reshaper_h(probs).tolist()
 
     def reshaper_h(self, tmps):
         ans = np.array([])
@@ -300,7 +309,6 @@ class Agent:
         for tmp in tmps:
             ans = np.vstack((ans, tmp)) if ans.size != 0 else np.array(tmp)
         return ans
-
 
     def play_n_step2(self):
         _states = [[] for _ in range(self.workers.num_workers)]

model.py
@@ -12,12 +12,12 @@ class Network(torch.nn.Module):
         # define network
         self.alpha = alpha
         self.beta = beta
-        self.c1 = nn.Conv2d(step_repeat_times, 32, 8, stride=4, padding=1)
-        self.c2 = nn.Conv2d(32, 64, 4, stride=2, padding=1)
-        self.c3 = nn.Conv2d(64, 64, 3, stride=1, padding=1)
+        self.c1 = nn.Conv2d(step_repeat_times, 32, 8, stride=4)
+        self.c2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.c3 = nn.Conv2d(64, 64, 3, stride=1)
         # self.c4 = nn.Conv2d(32, 32, 3, stride=2, padding=1)
         l5_out = 512
-        self.l5 = nn.Linear(6400, l5_out)  # 大きさ分からん
+        self.l5 = nn.Linear(3136, l5_out)  # 大きさ分からん
         self.critic = nn.Linear(l5_out, 1)
         self.actor = nn.Linear(l5_out, action_space)
 
@@ -63,7 +63,7 @@ class Network(torch.nn.Module):
 
     def select_action2(self, state):
         _, logits = self(state)
-        probs = F.softmax(logits, dim=0).to(dev)
+        probs = F.softmax(logits, dim=1).to(dev)
         c_rand = torch.distributions.categorical.Categorical(probs=probs.detach())  # detach はTensorから勾配を抜いた物
         act = c_rand.sample().cpu()
         # print("act {}".format(act))
@@ -91,7 +91,6 @@ class Network(torch.nn.Module):
         pis = torch.sum(acts_one_hot * probs, dim=1).to(dev)
         # print("pis shape {}.".format(pis.shape))
         log_pis = torch.log(pis).to(dev)  # log(pi(a_t|s_t)) (t=1,2,...)
-
         entropy = -torch.dot(pis, log_pis).to(dev)
         # print("d_res shape {}".format(d_rews.shape))
         # print("v_state shape {}".format(v_states.shape))
@@ -108,12 +107,11 @@ class Network(torch.nn.Module):
         return ans
 
     def calc_loss2(self, states, acts, d_rews):
-        acts_one_hot = torch.from_numpy(np.identity(4)[acts]).to(dev)  # one_hot ベクトルに変換
+        acts_one_hot = torch.from_numpy(np.identity(4, dtype=np.float)[acts]).to(dev)  # one_hot ベクトルに変換
         probs, crt = [], []
         for state in states:
             tmp = self(state)
-            prob = F.softmax(tmp[1], dim=0).to(dev)
-            probs.append(F.softmax(tmp[1], dim=0).to(dev)[0])
+            probs.append(F.softmax(tmp[1], dim=1).to(dev)[0])
             crt.append(tmp[0][0])
 
         probs = torch.stack(probs, dim=0).to(dev)