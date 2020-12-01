28f91d34692c0991ff1a2ed0b33cee6e7e4e28b0
Emile-Aquila <tmk8oakan6130@gmail.com>
2020-12-02 03:31:26 +0900

    clean codes

--- model.py
+++ model.py
@@ -40,56 +40,11 @@ class Network(torch.nn.Module):
         ans = self.critic(x), self.actor(x)
         return ans
 
-    def select_action(self, state):
-        _, logits = self(state)
-        probs = F.softmax(logits, dim=0).to(dev)
-        c_rand = torch.distributions.categorical.Categorical(probs=probs.detach())  # detach はTensorから勾配を抜いた物
-        act = c_rand.sample().cpu()
-        # print("act {}".format(act))
-        return act.numpy().copy(), probs
-
     def select_action2(self, state):
         _, logits = self(state)
         probs = F.softmax(logits, dim=1).to(dev)
         act = probs.multinomial(num_samples=1).cpu()[0]
         return act.numpy().copy()
-
-    def set_weight_on_network(self, g_net):  # 変数をセット(グローバルネットから取得した変数)
-        for param, g_param in zip(self.parameters(), g_net.parameters()):
-            if g_param.data is not None:
-                return
-            param.data = g_param.data.clone()
-
-    def calc_loss(self, states, acts, d_rews, probs):
-        acts_one_hot = torch.from_numpy(np.identity(4)[acts]).to(dev)  # one_hot ベクトルに変換
-        probs = torch.stack(probs, dim=0).to(dev)
-        d_rews = torch.stack(d_rews, dim=0).view(-1).to(dev)
-        tmp = [self(state)[0][0] for state in states]
-        v_states = torch.stack(tmp, dim=0).view(-1).to(dev)
-
-        # print("states size {}".format(v_states.shape))
-        # # print("acts size {}".format(acts.shape))
-        # print("d_rews size {}".format(d_rews.shape))
-        # print("probs size {}".format(probs.shape))
-
-        # pis = torch.dot(acts_one_hot, probs)  # pi(a_t|s_t) (t=1,2,...)
-        pis = torch.sum(acts_one_hot * probs, dim=1).to(dev)
-        # print("pis shape {}.".format(pis.shape))
-        log_pis = torch.log(pis).to(dev)  # log(pi(a_t|s_t)) (t=1,2,...)
-        entropy = -torch.dot(pis, log_pis).to(dev)
-        # print("d_res shape {}".format(d_rews.shape))
-        # print("v_state shape {}".format(v_states.shape))
-        advantage = d_rews - v_states
-        # print("adv shape {}".format(advantage.shape))
-        # print("log_pis {}".format(log_pis.shape))
-        v_loss = (advantage ** 2).to(dev)
-        a_loss = (log_pis * advantage).to(dev)
-        # print("a_loss shape {}".format(a_loss.shape))
-        ans = 0.5 * self.alpha * v_loss - a_loss - self.beta * entropy
-        ans = ans.to(dev)
-        ans = torch.sum(ans, dim=0).to(dev)
-
-        return ans
 
     def calc_loss2(self, states, acts, d_rews):
         probs, crt = [], []
